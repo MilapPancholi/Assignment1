82abc89ed817dbb5217715e3ce458ddcaa350900
Milap Chetankumar Pancholi <pancholimilap@gmail.com>
2019-09-30 21:26:46 -0400

    Final Commit

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -10,33 +10,45 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            bool IsExit = false;
+            bool IsExit = false,IsValidMenu=false;
             do
             {
                 int point1 = 0, point2 = 0, point3 = 0;
                 int MenuNo = 0;
                 Console.WriteLine("1. Enter Triangle Dimensions");
                 Console.WriteLine("2. Exit");
-                int.TryParse(Console.ReadLine(),out MenuNo);
+                IsValidMenu=int.TryParse(Console.ReadLine(),out MenuNo);
                 if (MenuNo == 1)
                 {
-                    Console.WriteLine("Enter Point 1:");
-                    int.TryParse(Console.ReadLine(), out point1);
+                    bool IsValidPoint1 = true;
+                    do
+                    {
+                        Console.WriteLine("Enter Point 1:");
+                        IsValidPoint1 = int.TryParse(Console.ReadLine(), out point1);
+                    } while (!IsValidPoint1);
 
-                    Console.WriteLine("Enter Point 2:");
-                    int.TryParse(Console.ReadLine(), out point2);
+                    bool IsValidPoint2 = true;
+                    do
+                    {
+                        Console.WriteLine("Enter Point 2:");
+                        IsValidPoint2=int.TryParse(Console.ReadLine(), out point2);
+                    } while (!IsValidPoint2);
 
-                    Console.WriteLine("Enter Point 3:");
-                    int.TryParse(Console.ReadLine(), out point3);
+                    bool IsValidPoint3 = true;
+                    do
+                    {
+                        Console.WriteLine("Enter Point 3:");
+                        IsValidPoint3 = int.TryParse(Console.ReadLine(), out point3);
+                    } while (!IsValidPoint3);
 
                     String Triangle=TriangleSolver.Analyze(point1,point2,point3);
                     Console.WriteLine(Triangle);
                 }
-                else
+                else if(MenuNo==2)
                 {
                     IsExit = true;   
                 }
-            } while (!IsExit);
+            } while (!IsExit || !IsValidMenu);
         }
     }
 }
